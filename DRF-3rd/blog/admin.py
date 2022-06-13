a6a73efee01b0f5e36fc572353d17f1f13c851ec
SWS2nd <thddnjstjr1@naver.com>
2022-06-14 00:07:43 +0900

    second commit

--- DRF-3rd/blog/admin.py
+++ DRF-3rd/blog/admin.py
@@ -3,7 +3,14 @@ from blog.models import Category as CategoryModel
 from blog.models import Article as ArticleModel
 
 
-# register Category Model in user app
-admin.site.register(CategoryModel)
-# register Article Model in user app
-admin.site.register(ArticleModel)
+class ArticleModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author']
+
+# ArticleModel 모델에 위에서 정의한 ArticleModelAdmin 설정을 가져다 쓸 것임
+admin.site.register(ArticleModel, ArticleModelAdmin)
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category_name']
+
+# CategoryModel 모델에 위에서 정의한 CategoryModelAdmin 설정을 가져다 쓸 것임
+admin.site.register(CategoryModel, CategoryModelAdmin)
--- DRF-3rd/blog/models.py
+++ DRF-3rd/blog/models.py
@@ -3,15 +3,26 @@ from user.models import User as UserModel
 
 
 class Category(models.Model):
-    category_name = models.CharField("카테고리 이름", max_length=20, unique=True)
+    category_name = models.CharField("카테고리", max_length=50)
     description = models.TextField()
+    
+    def __str__(self):
+        return self.category_name
 
 class Article(models.Model):
-    author = models.ForeignKey(to=UserModel, on_delete=models.CASCADE, related_name='author')
+    # 1:N = author:article 관계에서(유저 한명이 여러 게시글 작성 가능)
+    # on_delete 옵션 중
+    # CASCADE : 작성자(유저) 삭제시 게시글도 삭제
+    # PROTECT : 게시글 있으면 작성자(유저) 삭제 불가
+    # SET_DEFAULT(default 옵션을 꼭 추가해야 한다) : 작성자(유저) 삭제시 삭제된 작성자 대신 미리 지정해둔 임의의 값을 넣고 게시글 유지
+    author = models.ForeignKey(to=UserModel, verbose_name='작성자', related_name='author', on_delete=models.SET_DEFAULT, default='ghost')
     title = models.CharField("제목", max_length=200)
     # 참조해준 객체 입장에서 related_name을 설정
     category = models.ManyToManyField(to=Category, verbose_name='카테고리', related_name='category')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.title}"
     
