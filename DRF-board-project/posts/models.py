2cd388e09b451a8f63e731f83e649d1adc22a43f
SWS2nd <thddnjstjr1@naver.com>
2022-07-03 17:32:57 +0900

    create Post class & PostSerializer, PostCreateSerializer class in posts app

--- DRF-board-project/posts/models.py
+++ DRF-board-project/posts/models.py
@@ -1,3 +1,16 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User as UserModel
+from django.utils import timezone
+from users.models import Profile as ProfileModel
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='posts')
+    profile = models.ForeignKey(ProfileModel, on_delete=models.CASCADE, blank=True)
+    title = models.CharField(max_length=256)
+    category = models.CharField(max_length=256)
+    body = models.TextField()
+    image = models.ImageField(upload_to='post/', default='default.png')
+    likes = models.ManyToManyField(UserModel, related_name='like_posts', blank=True)
+    published_date = models.DateTimeField(default=timezone.now)
